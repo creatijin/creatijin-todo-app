10bae763bd83d2866ec127bcd41c836434b3a885
creatijin <newlex0124@gmail.com>
2019-03-28 12:19:22 +0900

    To Do layout + Design

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Creatijin Todo app!</Text>
-        <Text>React native app</Text>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>Creatijin To DO</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"}/>
+        </View>
       </View>
     );
   }
@@ -15,8 +21,36 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f45d11',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    margin: 50,
+    fontWeight: "200"
+  },
+  card: {
+    flex: 1,
+    backgroundColor: "white",
+    width: width - 50,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
