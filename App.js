cead75a9048d91dda143c09074e3f2934d9b4993
creatijin <newlex0124@gmail.com>
2019-03-31 15:01:31 +0900

    npm install uuid + addToDo

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, ScrollView } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import { AppLoading } from "expo";
 import ToDo from "./ToDo";
+import uuidv1 from "uuid/v1";
 
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   state = {
-    newToDo: ""
+    newToDo: "",
+    loadedToDos: false
   };
+  componentDidMount = () => {
+    this._loadToDos();
+  }
   render() {
-    const { newToDo } = this.state;
+    const { newToDo, loadedToDos } = this.state;
+    if (!loadedToDos) {
+      return <AppLoading/>
+    }
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content"/>
@@ -25,6 +34,7 @@ export default class App extends React.Component {
           placeholderTextColor={"#999"} 
           returnKeyType={"done"}
           autoCorrect={false}
+          onSubmitEditing={this._addToDo}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
             <ToDo text={"Hello I'm a To Do"}/>
@@ -36,8 +46,36 @@ export default class App extends React.Component {
   _crontollNewToDo = text => {
     this.setState({
       newToDo: text
+    });   
+  };
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
+    }); 
+  };
+  _addToDo = () => {
+    const { newToDo } = this.state;
+    this.setState(prevState => {
+      const ID = uuidv1();
+      const newToDoObject = {
+        [ID]: {
+          id: ID,
+          isCompleted: false,
+          text: newToDo,
+          createdAt: Date.now()
+        }
+      };
+      const newState = {
+        ...prevState,
+        newToDo: "",
+        toDos: {
+          ...prevState.toDos,
+          ...newToDoObject
+        }
+      }
+      return { ...newState };
     });
-  }
+  };
 }
 
 const styles = StyleSheet.create({
