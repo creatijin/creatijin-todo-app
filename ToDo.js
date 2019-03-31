553c45651067ba6966d6f5a5d9ca61024b48605b
creatijin <newlex0124@gmail.com>
2019-03-31 16:33:47 +0900

    delete action add

--- ToDo.js
+++ ToDo.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import PropTypes from "prop-types";
 
 const { height, width } = Dimensions.get("window");
 
 export default class ToDo extends Component {
-  state = {
-    isEditing: false,
-    isCompleted: false,
-    toDoValue: ""
-  };
+  constructor(props) {
+    super(props);
+    this.state = { isEditing: false, toDoValue: props.text };
+  }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    isCompleted: PropTypes.bool.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired
+  }
   render() {
     const { isCompleted, isEditing, toDoValue } = this.state;
-    const { text } = this.props;
+    const { text, id, deleteToDo } = this.props;
     return (
       <View style={styles.container}>
         <View style={styles.column}>
@@ -43,7 +49,7 @@ export default class ToDo extends Component {
         
           {isEditing ? (
             <View style={styles.action}>
-              <TouchableOpacity onPress={this._finishEditing}>
+              <TouchableOpacity onPressOut={this._finishEditing}>
                 <View style={styles.actionContainer}>
                   <Text style={styles.actionText}>✅</Text>
                 </View>
@@ -51,12 +57,12 @@ export default class ToDo extends Component {
             </View>
           ) : (
             <View style={styles.action}>
-              <TouchableOpacity onPress={this._startEditing}>
+              <TouchableOpacity onPressOut={this._startEditing}>
                 <View style={styles.actionContainer}>
                   <Text style={styles.actionText}>✏️</Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity>
+              <TouchableOpacity onPressOut={()=> deleteToDo(id)}>
                 <View style={styles.actionContainer}>
                   <Text style={styles.actionText}>❌</Text>
                 </View>
@@ -74,8 +80,7 @@ export default class ToDo extends Component {
     });
   };
   _startEditing = () => {
-    const { text } = this.props;
-    this.setState({ isEditing: true, toDoValue: text });
+    this.setState({ isEditing: true });
   };
   _finishEditing = () => {
     this.setState({
@@ -128,8 +133,7 @@ const styles = StyleSheet.create({
   column: {
     flexDirection: "row",
     alignItems: "center",
-    width: width / 2,
-    justifyContent: "space-between"
+    width: width / 2
   },
   action: {
     flexDirection: "row"
